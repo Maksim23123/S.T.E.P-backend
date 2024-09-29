3016d9394469471465357d2bf95aa8353c84d272
Maksym Boiko <maksym782@gmail.com>
2024-09-29 07:15:09 +0200

    Added Dtos and PackageService

---
+++ STEP backend/Architecture/Services/IPackageService.cs
@@ -0,0 +1,10 @@
+﻿using STEP_backend.Models.Dtos;
+
+namespace STEP_backend.Architecture.Services
+{
+    public interface IPackageService
+    {
+        Task CreatePackage(PackageDto package);
+        Task<IEnumerable<PackageDto>> GetAllPackagesOfStudentId(string studentId);
+    }
+}
---
+++ STEP backend/Models/Dtos/AnswerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace STEP_backend.Models.Dtos
+{
+    public class AnswerDto
+    {
+        public long Id { get; set; }
+        public string Text { get; set; }
+        public bool isCorrect { get; set; }
+
+    }
+}
