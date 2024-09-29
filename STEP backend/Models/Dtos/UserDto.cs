3efc3996173b89651e489f9549b4f0dd35ec3ba6
Maksym Boiko <maksym782@gmail.com>
2024-09-29 07:16:24 +0200

    Added AuthService

---
+++ STEP backend/Models/Dtos/UserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace STEP_backend.Models.Dtos
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string UserRole { get; set; }
+    }
+
+    public enum UserRoles
+    {
+        Teacher,
+        Student
+    }
+}
